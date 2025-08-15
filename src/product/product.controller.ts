f73e619c71037a3fc9578c4c2fa12eef49b088e7
Kim MiJu <20243100@office.deu.ac.kr>
2025-08-15 17:13:26 +0900

    product controller 주석 수정

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -7,26 +7,31 @@ import { UpdateProductDto } from './dto/update.product.request.dto';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  // 상품 생성
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
+  // 상품 조회 (전체)
   @Get()
   findAll() {
     return this.productService.findAll();
   }
 
+  // 상품 상세 조회 (상품 number)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id);
   }
 
+  // 상품 수정
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
+  // 상품 삭제
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
