042088ba61c71f2826fa0dbbb060f4981a307ea7
Kim MiJu <20243100@office.deu.ac.kr>
2025-08-15 17:05:10 +0900

    dto 파일 이름 수정

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { CreateProductDto } from './dto/create.product.request.dto';
+import { UpdateProductDto } from './dto/update.product.request.dto';
 
 @Injectable()
 export class ProductService {
