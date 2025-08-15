f3bc3dbdaffaf907fd1ef31d11ab776dce55faff
Kim MiJu <20243100@office.deu.ac.kr>
2025-08-15 16:38:59 +0900

    product entity 설정

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,19 @@
-export class Product {}
+import { Column, PrimaryGeneratedColumn } from "typeorm";
+
+export class Product {
+    @PrimaryGeneratedColumn()
+    number: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    des: string;
+
+    @Column()
+    stock: number;
+
+    @Column()
+    price: number;
+
+}
