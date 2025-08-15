c9d55fdc8451e630eb8e08431400bc4a8e088afd
Kim MiJu <20243100@office.deu.ac.kr>
2025-08-15 17:13:37 +0900

    sign_up 로직 생성

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,32 +1,37 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create.user.request.dto';
+import { UpdateUserDto } from './dto/update.user.request.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // 유저 생성
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  sign_up(@Body() createUserDto: CreateUserDto) {
+    return this.userService.sign_up(createUserDto);
   }
 
+  // 유저 조회 (전체)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  // 유저 상세 조회 (유저 id)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  // 유저 수정 (유저 id)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
+  // 유저 삭제
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create.user.request.dto';
+import { UpdateUserDto } from './dto/update.user.request.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -12,24 +12,27 @@ export class UserService {
     private readonly userRepository: Repository<User>
   ){}
 
-  create(createUserDto: CreateUserDto) {
-    await 
-    return;
+  async sign_up(createUserDto: CreateUserDto): Promise<void> {
+    const exist = await this.userRepository.findOne({ 
+      where: { id : createUserDto.id } });
+    if(exist) throw new BadRequestException('이미 존재하는 ID 입니다.')
+    const user = this.userRepository.create(createUserDto);
+    await this.userRepository.save(user);
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} user`;
   }
 }
