dd1239dd9c7d6471685e69dfaac48642fbb9c19d
Kim MiJu <20243100@office.deu.ac.kr>
2025-08-16 23:30:57 +0900

    Swagger 설치 후 초기 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js Study - Kurly API Docs')
+    .setDescription('2025 여름방학 Nest.js 스터디 과제 - Kurly API')
+    .setVersion('1.0')
+    .addTag('kurly')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
